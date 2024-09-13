9827f159eb1de15de5ac281cd39c6eb5b9b340bc
王文龙 <2641090294@qq.com>
2024-09-13 18:33:17 +0800

    9.13 上传修改后的demo

--- PrimaryMemoryTable/C/Windows/Demo/TimeBase/main.c
+++ PrimaryMemoryTable/C/Windows/Demo/TimeBase/main.c
@@ -40,7 +40,7 @@ uint8_t uart_init(HANDLE hSerial)
 }
 
 //串口发送
-uint8_t order_send(HANDLE hSerial, uint8_t *order_buffer,uint8_t order_len)
+uint8_t order_send(HANDLE hSerial, uint8_t* order_buffer, uint8_t order_len)
 {
     uint8_t ret;                    //状态标志位
     DWORD bytesWritten;             //实际写入数据长度
@@ -48,7 +48,7 @@ uint8_t order_send(HANDLE hSerial, uint8_t *order_buffer,uint8_t order_len)
     //写入串口数据
     ret = WriteFile(hSerial, order_buffer, order_len, &bytesWritten, NULL);
 
-    if(ret != 0)
+    if (ret != 0)
     {
         return TRUE;
     }
@@ -78,9 +78,9 @@ uint8_t order_receive(HANDLE hSerial, uint8_t pack[])
     //读取串口缓冲区数据
     ret = ReadFile(hSerial, pack, read_len, &bytesRead, NULL);
 
-    if(ret != 0)
+    if (ret != 0)
     {
-        if(bytesRead > 0)
+        if (bytesRead > 0)
         {
             return TRUE;
         }
@@ -99,88 +99,71 @@ uint8_t order_receive(HANDLE hSerial, uint8_t pack[])
 
 int main() {
 
-    uint8_t order_buffer[20] = {0};                                                                         //存放生成的指令
+    uint8_t order_buffer[20] = { 0 };                                                                         //存放生成的指令
     uint8_t order_len = 0;                                                                                  //指令长度
-    uint8_t pack[20] = {0};                                                                                 //存放接收的应答包
+    uint8_t pack[20] = { 0 };                                                                                 //存放接收的应答包
     uint8_t ret;
+    uint8_t write_buffer[20] = { 0 };                                                                         //写入内存表数据
+    struct servo_sync_parameter servo;
 
     // 打开串口
-    HANDLE hSerial = CreateFile("COM3", GENERIC_READ | GENERIC_WRITE, 0, NULL,
-                                OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    HANDLE hSerial = CreateFile("\\\\.\\COM16", GENERIC_READ | GENERIC_WRITE, 0, NULL,
+        OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
     //串口初始化
     ret = uart_init(hSerial);
-    if(ret == FALSE)
+    if (ret == FALSE)
     {
         return FALSE;
     }
 
-    //设置舵机的扭矩开关
-    servo_set_torque_switch(1, 0, order_buffer,&order_len);
+    servo.id_counts = 2;            //同步写两个舵机
+    servo.id[0] = 1;                //第一个舵机id为1
+    servo.id[1] = 2;                //第二个舵机id为2
 
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
+    //将ID1、ID2舵机的扭矩开关状态，分别修改为关闭
+    servo.torque_switch[0] = 0;
+    servo.torque_switch[1] = 0;
+    servo_sync_write_torque_switch(servo, order_buffer, &order_len);
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
     {
         return FALSE;
     }
-    Sleep(1);
-
-    ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
+    else
     {
-        return FALSE;
-    }
-    Sleep(80);
-    servo_set_torque_switch_analysis(pack);
-
-    //设置舵机的控制模式
-    servo_set_control_mode(1, 0, order_buffer,&order_len);
-
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
-    {
-        return FALSE;
-    }
-    Sleep(1);
-
-    ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
-    {
-        return FALSE;
+        PRINTF("Sync Write torque witch successfully.\r\n");
     }
     Sleep(80);
-    servo_set_control_mode_analysis(pack);
 
-    //设置舵机的扭矩开关
-    servo_set_torque_switch(1, 1, order_buffer,&order_len);
-
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
+    //将ID1、ID2舵机的控制模式，分别修改为控shi模式
+    servo.control_mode[0] = 0;
+    servo.control_mode[1] = 0;
+    servo_sync_write_control_mode(servo, order_buffer, &order_len);
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
     {
         return FALSE;
     }
-    Sleep(1);
-
-    ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
+    else
     {
-        return FALSE;
+        PRINTF("Sync Write control mode successfully.\r\n");
     }
     Sleep(80);
-    servo_set_torque_switch_analysis(pack);
 
-    //设置舵机的控时目标位置和目标运行时间
-    servo_set_time_base_target_position_and_moving_time(1, 3000, 500, order_buffer,&order_len);
 
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
+    //在控时模式下，让ID1舵机以500ms运动到300°位置
+    servo_set_time_base_target_position_and_moving_time(1, 3000, 500, order_buffer, &order_len);
+
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
     {
         return FALSE;
     }
     Sleep(1);
 
     ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
+    if (ret == FALSE)
     {
         return FALSE;
     }
@@ -188,42 +171,60 @@ int main() {
 
     servo_set_time_base_target_position_and_moving_time_analysis(pack);
 
-    //设置舵机的控时目标加速度等级
-    servo_set_time_base_target_acc(1, 0, order_buffer,&order_len);
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
+    //在控时模式下，让ID1舵机以1s匀速运动到0°位置
+    servo_set_time_base_target_position_and_moving_time(1, 0, 1000, order_buffer, &order_len);
+
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
     {
         return FALSE;
     }
     Sleep(1);
 
     ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
-    {
-        return FALSE;
-    }
-    Sleep(80);
-
-    servo_set_time_base_target_acc_analysis(pack);
-
-    //设置舵机的控时目标位置和目标运行时间
-    servo_set_time_base_target_position_and_moving_time(1, 0, 1000, order_buffer,&order_len);
-
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
-    {
-        return FALSE;
-    }
-    Sleep(1);
-
-    ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
+    if (ret == FALSE)
     {
         return FALSE;
     }
     Sleep(1000);
 
     servo_set_time_base_target_position_and_moving_time_analysis(pack);
+
+    //在控时模式下，让ID1舵机以500ms运动到150°位置，让ID2舵机以1s匀速运动到0°位置
+    servo.position[0] = 1500;
+    servo.position[1] = 0;
+    servo.time[0] = 500;
+    servo.time[1] = 1000;
+
+    servo_sync_write_time_base_target_position_and_moving_time(servo, order_buffer, &order_len);
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
+    {
+        return FALSE;
+    }
+    else
+    {
+        PRINTF("Sync Write time base target position and moving time successfully.\r\n");
+    }
+    Sleep(1000);
+
+    //在控时模式下，让ID1舵机以1000ms运动到0°位置，让ID2舵机以500ms匀速运动到3000°位置
+    servo.position[0] = 0;
+    servo.position[1] = 3000;
+    servo.time[0] = 1000;
+    servo.time[1] = 500;
+
+    servo_sync_write_time_base_target_position_and_moving_time(servo, order_buffer, &order_len);
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
+    {
+        return FALSE;
+    }
+    else
+    {
+        PRINTF("Sync Write time base target position and moving time successfully.\r\n");
+    }
+    Sleep(1000);
 
     //关闭串口
     CloseHandle(hSerial);
--- PrimaryMemoryTable/C/Windows/Demo/VelocityBase/main.c
+++ PrimaryMemoryTable/C/Windows/Demo/VelocityBase/main.c
@@ -40,7 +40,7 @@ uint8_t uart_init(HANDLE hSerial)
 }
 
 //串口发送
-uint8_t order_send(HANDLE hSerial, uint8_t *order_buffer,uint8_t order_len)
+uint8_t order_send(HANDLE hSerial, uint8_t* order_buffer, uint8_t order_len)
 {
     uint8_t ret;                    //状态标志位
     DWORD bytesWritten;             //实际写入数据长度
@@ -48,7 +48,7 @@ uint8_t order_send(HANDLE hSerial, uint8_t *order_buffer,uint8_t order_len)
     //写入串口数据
     ret = WriteFile(hSerial, order_buffer, order_len, &bytesWritten, NULL);
 
-    if(ret != 0)
+    if (ret != 0)
     {
         return TRUE;
     }
@@ -78,11 +78,11 @@ uint8_t order_receive(HANDLE hSerial, uint8_t pack[])
     //读取串口缓冲区数据
     ret = ReadFile(hSerial, pack, read_len, &bytesRead, NULL);
 
-    if(ret != 0)
+    if (ret != 0)
     {
-        if(bytesRead > 0)
+        if (bytesRead > 0)
         {
-            return TRUE;
+            return bytesRead;
         }
         else
         {
@@ -99,105 +99,71 @@ uint8_t order_receive(HANDLE hSerial, uint8_t pack[])
 
 int main() {
 
-    uint8_t order_buffer[20] = {0};                                                                         //存放生成的指令
+    uint8_t order_buffer[40] = { 0 };                                                                         //存放生成的指令
     uint8_t order_len = 0;                                                                                  //指令长度
-    uint8_t pack[20] = {0};                                                                                 //存放接收的应答包
+    uint8_t pack[20] = { 0 };                                                                                 //存放接收的应答包
     uint8_t ret;
+    uint8_t write_buffer[20] = { 0 };                                                                         //写入内存表数据
+    
+    struct servo_sync_parameter servo;
+
+    servo.id_counts = 2;            //同步写两个舵机
+    servo.id[0] = 1;                //第一个舵机id为1
+    servo.id[1] = 2;                //第二个舵机id为2
 
     // 打开串口
-    HANDLE hSerial = CreateFile("COM3", GENERIC_READ | GENERIC_WRITE, 0, NULL,
-                                OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    HANDLE hSerial = CreateFile("\\\\.\\COM16", GENERIC_READ | GENERIC_WRITE, 0, NULL,
+        OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
     //串口初始化
     ret = uart_init(hSerial);
-    if(ret == FALSE)
+    if (ret == FALSE)
     {
         return FALSE;
     }
 
-    //参数重置
-    servo_parameter_reset(1, order_buffer,&order_len);
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
+    //将ID1、ID2舵机的扭矩开关状态，分别修改为关闭
+    servo.torque_switch[0] = 0;
+    servo.torque_switch[1] = 0;
+    servo_sync_write_torque_switch(servo, order_buffer, &order_len);
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
     {
         return FALSE;
     }
-    Sleep(1);
-
-    ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
+    else
     {
-        return FALSE;
-    }
-    Sleep(80);
-    servo_parameter_reset_analysis(pack);
-
-    //设置舵机的扭矩开关
-    servo_set_torque_switch(1, 0, order_buffer,&order_len);
-
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
-    {
-        return FALSE;
-    }
-    Sleep(1);
-
-    ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
-    {
-        return FALSE;
+        PRINTF("Sync Write torque witch successfully.\r\n");
     }
     Sleep(80);
-    servo_set_torque_switch_analysis(pack);
 
-    //设置舵机的控制模式
-    servo_set_control_mode(1, 1, order_buffer,&order_len);
-
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
+    //将ID1、ID2舵机的控制模式，分别修改为控速模式
+    servo.control_mode[0] = 1;
+    servo.control_mode[1] = 1;
+    servo_sync_write_control_mode(servo, order_buffer, &order_len);
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
     {
         return FALSE;
     }
-    Sleep(1);
-
-    ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
+    else
     {
-        return FALSE;
+        PRINTF("Sync Write control mode successfully.\r\n");
     }
     Sleep(80);
-    servo_set_control_mode_analysis(pack);
-
-    //设置舵机的扭矩开关
-    servo_set_torque_switch(1, 1, order_buffer,&order_len);
-
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
-    {
-        return FALSE;
-    }
-    Sleep(1);
-
-    ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
-    {
-        return FALSE;
-    }
-    Sleep(80);
-    servo_set_torque_switch_analysis(pack);
 
     //设置舵机的控速目标位置
-    servo_set_velocity_base_target_position(1, 1500, order_buffer,&order_len);
+    servo_set_velocity_base_target_position(1, 1500, order_buffer, &order_len);
 
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
     {
         return FALSE;
     }
     Sleep(1);
 
     ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
+    if (ret == FALSE)
     {
         return FALSE;
     }
@@ -205,85 +171,127 @@ int main() {
 
     servo_set_velocity_base_target_position_analysis(pack);
 
-    //设置舵机的控速目标速度
-    servo_set_velocity_base_target_velocity(1, 3600, order_buffer,&order_len);
+    //在控速模式下，让ID1以360°/s的控速目标速度运动到300°位置
+    write_buffer[0] = 3000 & 0xff;
+    write_buffer[1] = (3000 >> 8) & 0xff;
+    write_buffer[2] = 3600 & 0xff;
+    write_buffer[3] = (3600 >> 8) & 0xff;
 
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
+    servo_write(1, 0x35, 4, write_buffer, order_buffer, &order_len);
+
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
     {
         return FALSE;
     }
     Sleep(1);
 
     ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
+    if (ret == FALSE)
     {
         return FALSE;
     }
-    Sleep(80);
-
-    servo_set_velocity_base_target_velocity_analysis(pack);
-
-    //设置舵机的控速目标加速度
-    servo_set_velocity_base_target_acc(1, 10, order_buffer,&order_len);
-
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
+    PRINTF("servo pack is: ");
+    for (uint8_t i = 0; i < ret; i++)
     {
-        return FALSE;
+        PRINTF("0x%x ", pack[i]);
     }
-    Sleep(1);
+    PRINTF("\r\n");
+    Sleep(1000);
 
-    ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
-    {
-        return FALSE;
-    }
-    Sleep(80);
+    //在控速模式下，让ID1舵机以360°/s的控速目标速度、500°/s²的控速目标加速度、50°/s²的控速目标减速度运动到0°位置
+    write_buffer[0] = 0 & 0xff;
+    write_buffer[1] = (0 >> 8) & 0xff;
+    write_buffer[2] = 3600 & 0xff;
+    write_buffer[3] = (3600 >> 8) & 0xff;
+    write_buffer[4] = 10 & 0xff;
+    write_buffer[5] = 1 & 0xff;
 
-    servo_set_velocity_base_target_acc_analysis(pack);
+    servo_write(1, 0x35, 6, write_buffer, order_buffer, &order_len);
 
-    //设置舵机的控速目标减速度
-    servo_set_velocity_base_target_dec(1, 1, order_buffer,&order_len);
-
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
     {
         return FALSE;
     }
     Sleep(1);
 
     ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
+    if (ret == FALSE)
     {
         return FALSE;
     }
-    Sleep(80);
+    PRINTF("servo pack is: ");
+    for (uint8_t i = 0; i < ret; i++)
+    {
+        PRINTF("0x%x ", pack[i]);
+    }
+    PRINTF("\r\n");
+    Sleep(1000);
 
-    servo_set_velocity_base_target_dec_analysis(pack);
+    //在控速模式下，让ID1舵机运动到150中位°，让ID2舵机运动到0°位置
 
-    //设置舵机的控速目标位置
-    servo_set_velocity_base_target_position(1, 0, order_buffer,&order_len);
+    //id为1，2的舵机运动位置分别设置为1500，0，值和前面的id设置对应
+    servo.position[0] = 1500;
+    servo.position[1] = 0;
 
-    ret = order_send(hSerial, order_buffer,order_len);
-    if(ret == FALSE)
+    servo_sync_write_velocity_base_target_position(servo, order_buffer, &order_len);
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
     {
         return FALSE;
     }
-    Sleep(1);
-
-    ret = order_receive(hSerial, pack);
-    if(ret == FALSE)
+    else
     {
-        return FALSE;
+        PRINTF("Sync Write velocity base target position successfully.\r\n");
     }
     Sleep(1000);
 
-    servo_set_velocity_base_target_position_analysis(pack);
+    //在控速模式下，让ID1以360°/s的控速目标速度运动到300°位置，让ID2以720°/s的控速目标速度运动到150°位置
 
+    //id为1，2的舵机速度分别设置为3600，7200，位置分别设置为3000，1500
+    servo.velocity[0] = 3600;
+    servo.velocity[1] = 7200;
+    servo.position[0] = 3000;
+    servo.position[1] = 1500;
 
+    servo_sync_write_velocity_base_target_position_and_velocity(servo, order_buffer, &order_len);
 
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
+    {
+        return FALSE;
+    }
+    else
+    {
+        PRINTF("Sync Write velocity base target position and velocity successfully.\r\n");
+    }
+    Sleep(1000);
 
+    //在控速模式下，让ID1舵机以720°/s的控速目标速度、500°/s²的控速目标加速度、50°/s²的控速目标减速度运动到0°位置，让ID2舵机以360°/s的控速目标速度、50°/s²的控速目标加速度、500°/s²的控速目标减速度运动到300°位置
+
+    //id为1，2的舵机速度分别设置为7200，3600，位置分别设置为0，3000,加速度分别设置为10，1，减速度分别设置为1，10
+    servo.velocity[0] = 7200;
+    servo.velocity[1] = 3600;
+    servo.position[0] = 0;
+    servo.position[1] = 3000;
+    servo.acc_velocity[0] = 10;
+    servo.acc_velocity[1] = 1;
+    servo.dec_velocity[0] = 1;
+    servo.dec_velocity[1] = 10;
+
+    servo_sync_write_velocity_base_target_acc_dec_velocity_and_position(servo, order_buffer, &order_len);
+
+    ret = order_send(hSerial, order_buffer, order_len);
+    if (ret == FALSE)
+    {
+        return FALSE;
+    }
+    else
+    {
+        PRINTF("Sync Write velocity base target acc,dec,velocity and position successfully.\r\n");
+    }
+    Sleep(1000);
 
     //关闭串口
     CloseHandle(hSerial);
