f6ca96488412ca2c940e3c3f40a0a8a9159aa9af
王文龙 <2641090294@qq.com>
2024-09-13 19:00:43 +0800

    9.13 修改C++的控时和控速demo

--- PrimaryMemoryTable/C++/Windows/Demo/TimeBase/main.cpp
+++ PrimaryMemoryTable/C++/Windows/Demo/TimeBase/main.cpp
@@ -4,10 +4,16 @@
 
 int main()
 {
-    uint8_t ret;
-    uint8_t order_buffer[20] = {0};                                                                         //存放生成的指令
-    uint8_t order_len = 0;                                                                                  //指令长度
-    uint8_t pack[20] = {0};                                                                                 //存放接收的应答包
+    uint8_t ret;          
+    uint8_t order_buffer[20] = { 0 };                               //存放生成的指令
+    uint8_t order_len = 0;                                             //指令长度
+    uint8_t pack[20] = { 0 };                                         //存放接收的应答包
+    uint8_t write_buffer[20] = { 0 };                              //写入内存表数据   
+    struct servo_sync_parameter servo;
+
+    servo.id_counts = 2;            //同步写两个舵机
+    servo.id[0] = 1;                //第一个舵机id为1
+    servo.id[1] = 2;                //第二个舵机id为2
 
     //创建串口的控制类
     CSerialPort serialPort;
@@ -17,7 +23,7 @@ int main()
     //实际串口写入的字节数
     DWORD bytesWritten;
 
-    if (serialPort.Open(14, 1000000))
+    if (serialPort.Open(16, 1000000))
     {
         PRINTF("\r\nOpen Serial successfully.");
     }
@@ -28,80 +34,38 @@ int main()
         return -1;
     }
 
-    //设置舵机的扭矩开关
-    servo_set_torque_switch(1, 0, order_buffer,&order_len);
+    //将ID1、ID2舵机的扭矩开关状态，分别修改为关闭
+    servo.torque_switch[0] = 0;
+    servo.torque_switch[1] = 0;
+    servo_sync_write_torque_switch(servo, order_buffer, &order_len);
 
-    if (serialPort.Write(order_buffer, order_len, &bytesWritten)) {
-        PRINTF("\r\nWrite successfully.");
+    if (serialPort.Write(order_buffer, order_len, &bytesWritten))
+    {
+        PRINTF("\r\nSync Write torque witch successfully.");
     }
     else
     {
         PRINTF("\r\nFailed to send data.");
-    }
-    Sleep(1);
-
-    if (serialPort.Read(pack,&bytesRead))
-    {
-        ret = servo_set_torque_switch_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to read data.");
     }
     Sleep(20);
 
-    //设置舵机的控制模式
-    servo_set_control_mode(1, 0, order_buffer,&order_len);
+    //将ID1、ID2舵机的控制模式，分别修改为控时模式
+    servo.control_mode[0] = 0;
+    servo.control_mode[1] = 0;
+    servo_sync_write_control_mode(servo, order_buffer, &order_len);
 
-    if (serialPort.Write(order_buffer, order_len, &bytesWritten)) {
-        PRINTF("\r\nWrite successfully.");
+    if (serialPort.Write(order_buffer, order_len, &bytesWritten))
+    {
+        PRINTF("\r\nSync Write control mode successfully.");
     }
     else
     {
         PRINTF("\r\nFailed to send data.");
-    }
-    Sleep(1);
-
-    if (serialPort.Read(pack,&bytesRead))
-    {
-        ret = servo_set_control_mode_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to read data.");
-    }
-    Sleep(20);
-
-    //设置舵机的扭矩开关
-    servo_set_torque_switch(1, 1, order_buffer,&order_len);
-
-    if (serialPort.Write(order_buffer, order_len, &bytesWritten)) {
-        PRINTF("\r\nWrite successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to send data.");
-    }
-    Sleep(1);
-
-    if (serialPort.Read(pack,&bytesRead))
-    {
-        ret = servo_set_torque_switch_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to read data.");
     }
     Sleep(20);
 
     //设置舵机的控时目标位置和目标运行时间
-    servo_set_time_base_target_position_and_moving_time(1, 3000, 500, order_buffer,&order_len);
+    servo_set_time_base_target_position_and_moving_time(1, 3000, 500, order_buffer, &order_len);
     if (serialPort.Write(order_buffer, order_len, &bytesWritten))
     {
         PRINTF("\r\nWrite successfully.");
@@ -112,10 +76,10 @@ int main()
     }
     Sleep(1);
 
-    if (serialPort.Read(pack,&bytesRead))
+    if (serialPort.Read(pack, &bytesRead))
     {
         ret = servo_set_time_base_target_position_and_moving_time_analysis(pack);
-        if(ret == SUCCESS)
+        if (ret == SUCCESS)
             PRINTF("\r\nSet successfully.");
     }
     else
@@ -124,33 +88,8 @@ int main()
     }
     Sleep(1000);
 
-    //设置舵机的控时目标加速度等级
-    servo_set_time_base_target_acc(1, 0, order_buffer,&order_len);
-
-    if (serialPort.Write(order_buffer, order_len, &bytesWritten))
-    {
-        PRINTF("\r\nWrite successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to send data.");
-    }
-    Sleep(1);
-
-    if (serialPort.Read(pack,&bytesRead))
-    {
-        ret = servo_set_time_base_target_acc_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to read data.");
-    }
-    Sleep(20);
-
     //设置舵机的控时目标位置和目标运行时间
-    servo_set_time_base_target_position_and_moving_time(1, 0, 1000, order_buffer,&order_len);
+    servo_set_time_base_target_position_and_moving_time(1, 0, 1000, order_buffer, &order_len);
     if (serialPort.Write(order_buffer, order_len, &bytesWritten))
     {
         PRINTF("\r\nWrite successfully.");
@@ -161,15 +100,51 @@ int main()
     }
     Sleep(1);
 
-    if (serialPort.Read(pack,&bytesRead))
+    if (serialPort.Read(pack, &bytesRead))
     {
         ret = servo_set_time_base_target_position_and_moving_time_analysis(pack);
-        if(ret == SUCCESS)
+        if (ret == SUCCESS)
             PRINTF("\r\nSet successfully.");
     }
     else
     {
         PRINTF("\r\nFailed to read data.");
+    }
+    Sleep(1000);
+
+    //在控时模式下，让ID1舵机以500ms运动到150°位置，让ID2舵机以1s匀速运动到0°位置
+
+    //设置舵机id为1，2的运动位置为3000，3000，运动时间为500ms，1500ms
+    servo.position[0] = 3000;
+    servo.position[1] = 3000;
+    servo.time[0] = 500;
+    servo.time[1] = 1500;
+
+    servo_sync_write_time_base_target_position_and_moving_time(servo, order_buffer, &order_len);
+    if (serialPort.Write(order_buffer, order_len, &bytesWritten))
+    {
+        PRINTF("\r\nSync Write time base target position and moving time successfully.");
+    }
+    else
+    {
+        PRINTF("\r\nFailed to send data.");
+    }
+    Sleep(1000);
+
+    //设置舵机id为1，2的运动位置为0，3000，运动时间为1000ms，500ms
+    servo.position[0] = 0;
+    servo.position[1] = 3000;
+    servo.time[0] = 1000;
+    servo.time[1] = 500;
+
+    servo_sync_write_time_base_target_position_and_moving_time(servo, order_buffer, &order_len);
+    if (serialPort.Write(order_buffer, order_len, &bytesWritten))
+    {
+        PRINTF("\r\nSync Write time base target position and moving time successfully.");
+    }
+    else
+    {
+        PRINTF("\r\nFailed to send data.");
     }
     Sleep(1000);
 
--- PrimaryMemoryTable/C++/Windows/Demo/VelocityBase/main.cpp
+++ PrimaryMemoryTable/C++/Windows/Demo/VelocityBase/main.cpp
@@ -5,9 +5,16 @@
 int main()
 {
     uint8_t ret;
-    uint8_t order_buffer[20] = {0};                                                                         //存放生成的指令
+    uint8_t order_buffer[40] = { 0 };                                                                         //存放生成的指令
     uint8_t order_len = 0;                                                                                  //指令长度
-    uint8_t pack[20] = {0};                                                                                 //存放接收的应答包
+    uint8_t pack[20] = { 0 };                                                                                 //存放接收的应答包
+    uint8_t write_buffer[20] = { 0 };                              //写入内存表数据  
+
+    struct servo_sync_parameter servo;
+
+    servo.id_counts = 2;            //同步写两个舵机
+    servo.id[0] = 1;                //第一个舵机id为1
+    servo.id[1] = 2;                //第二个舵机id为2
 
     //创建串口的控制类
     CSerialPort serialPort;
@@ -17,7 +24,7 @@ int main()
     //实际串口写入的字节数
     DWORD bytesWritten;
 
-    if (serialPort.Open(14, 1000000))
+    if (serialPort.Open(16, 1000000))
     {
         PRINTF("\r\nOpen Serial successfully.");
     }
@@ -28,106 +35,38 @@ int main()
         return -1;
     }
 
-    //参数重置
-    servo_parameter_reset(1,order_buffer, &order_len);
+    //将ID1、ID2舵机的扭矩开关状态，分别修改为关闭
+    servo.torque_switch[0] = 0;
+    servo.torque_switch[1] = 0;
+    servo_sync_write_torque_switch(servo, order_buffer, &order_len);
+
     if (serialPort.Write(order_buffer, order_len, &bytesWritten))
     {
-        PRINTF("\r\nData sent successfully.");
+        PRINTF("Sync Write torque witch successfully.\r\n");
     }
     else
     {
         PRINTF("\r\nFailed to send data.");
-    }
-    Sleep(1);
-
-    if (serialPort.Read(pack,&bytesRead))
-    {
-        ret = servo_parameter_reset_analysis(pack);
-        if(ret == SUCCESS)
-        {
-            PRINTF("\r\nservo parameter reset successfully!");
-        }
-    }
-    else
-    {
-        PRINTF("\r\nFailed to read data.");
     }
     Sleep(20);
 
-    //设置舵机的扭矩开关
-    servo_set_torque_switch(1, 0, order_buffer,&order_len);
+    //将ID1、ID2舵机的控制模式，分别修改为控速模式
+    servo.control_mode[0] = 1;
+    servo.control_mode[1] = 1;
+    servo_sync_write_control_mode(servo, order_buffer, &order_len);
 
-    if (serialPort.Write(order_buffer, order_len, &bytesWritten)) {
-        PRINTF("\r\nWrite successfully.");
+    if (serialPort.Write(order_buffer, order_len, &bytesWritten))
+    {
+        PRINTF("Sync Write control mode successfully.\r\n");
     }
     else
     {
         PRINTF("\r\nFailed to send data.");
-    }
-    Sleep(1);
-
-    if (serialPort.Read(pack,&bytesRead))
-    {
-        ret = servo_set_torque_switch_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to read data.");
-    }
-    Sleep(20);
-
-    //设置舵机的控制模式
-    servo_set_control_mode(1, 1, order_buffer,&order_len);
-
-    if (serialPort.Write(order_buffer, order_len, &bytesWritten)) {
-        PRINTF("\r\nWrite successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to send data.");
-    }
-    Sleep(1);
-
-    if (serialPort.Read(pack,&bytesRead))
-    {
-        ret = servo_set_control_mode_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to read data.");
-    }
-    Sleep(20);
-
-    //设置舵机的扭矩开关
-    servo_set_torque_switch(1, 1, order_buffer,&order_len);
-
-    if (serialPort.Write(order_buffer, order_len, &bytesWritten)) {
-        PRINTF("\r\nWrite successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to send data.");
-    }
-    Sleep(1);
-
-    if (serialPort.Read(pack,&bytesRead))
-    {
-        ret = servo_set_torque_switch_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to read data.");
     }
     Sleep(20);
 
     //设置舵机的控速目标位置
-    servo_set_velocity_base_target_position(1, 1500, order_buffer,&order_len);
+    servo_set_velocity_base_target_position(1, 1500, order_buffer, &order_len);
     if (serialPort.Write(order_buffer, order_len, &bytesWritten)) {
         PRINTF("\r\nWrite successfully.");
     }
@@ -137,10 +76,10 @@ int main()
     }
     Sleep(1);
 
-    if (serialPort.Read(pack,&bytesRead))
+    if (serialPort.Read(pack, &bytesRead))
     {
         ret = servo_set_velocity_base_target_position_analysis(pack);
-        if(ret == SUCCESS)
+        if (ret == SUCCESS)
             PRINTF("\r\nSet successfully.");
     }
     else
@@ -149,8 +88,13 @@ int main()
     }
     Sleep(1000);
 
-    //设置舵机的控速目标速度
-    servo_set_velocity_base_target_velocity(1, 3600, order_buffer,&order_len);
+    //在控速模式下，让ID1以360°/s的控速目标速度运动到300°位置
+    write_buffer[0] = 3000 & 0xff;
+    write_buffer[1] = (3000 >> 8) & 0xff;
+    write_buffer[2] = 3600 & 0xff;
+    write_buffer[3] = (3600 >> 8) & 0xff;
+
+    servo_write(1, 0x35, 4, write_buffer, order_buffer, &order_len);
 
     if (serialPort.Write(order_buffer, order_len, &bytesWritten))
     {
@@ -162,20 +106,30 @@ int main()
     }
     Sleep(1);
 
-    if (serialPort.Read(pack,&bytesRead))
+    if (serialPort.Read(pack, &bytesRead))
     {
-        ret = servo_set_velocity_base_target_velocity_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
+        PRINTF("servo pack is: ");
+        for (uint8_t i = 0; i < bytesRead; i++)
+        {
+            PRINTF("0x%x ", pack[i]);
+        }
+        PRINTF("\r\n");
     }
     else
     {
         PRINTF("\r\nFailed to read data.");
     }
-    Sleep(20);
+    Sleep(1000);
 
-    //设置舵机的控速目标加速度
-    servo_set_velocity_base_target_acc(1, 10, order_buffer,&order_len);
+    //在控速模式下，让ID1舵机以360°/s的控速目标速度、500°/s²的控速目标加速度、50°/s²的控速目标减速度运动到0°位置
+    write_buffer[0] = 0 & 0xff;
+    write_buffer[1] = (0 >> 8) & 0xff;
+    write_buffer[2] = 3600 & 0xff;
+    write_buffer[3] = (3600 >> 8) & 0xff;
+    write_buffer[4] = 10 & 0xff;
+    write_buffer[5] = 1 & 0xff;
+
+    servo_write(1, 0x35, 6, write_buffer, order_buffer, &order_len);
 
     if (serialPort.Write(order_buffer, order_len, &bytesWritten))
     {
@@ -187,63 +141,77 @@ int main()
     }
     Sleep(1);
 
-    if (serialPort.Read(pack,&bytesRead))
+    if (serialPort.Read(pack, &bytesRead))
     {
-        ret = servo_set_velocity_base_target_acc_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
+        PRINTF("servo pack is: ");
+        for (uint8_t i = 0; i < bytesRead; i++)
+        {
+            PRINTF("0x%x ", pack[i]);
+        }
+        PRINTF("\r\n");
     }
     else
     {
         PRINTF("\r\nFailed to read data.");
     }
-    Sleep(20);
+    Sleep(1000);
 
-    //设置舵机的控速目标减速度
-    servo_set_velocity_base_target_dec(1, 1, order_buffer,&order_len);
+    //在控速模式下，让ID1舵机运动到150中位°，让ID2舵机运动到0°位置
 
+    //id为1，2的舵机运动位置分别设置为1500，0，值和前面的id设置对应
+    servo.position[0] = 1500;
+    servo.position[1] = 0;
+
+    servo_sync_write_velocity_base_target_position(servo, order_buffer, &order_len);
     if (serialPort.Write(order_buffer, order_len, &bytesWritten))
     {
-        PRINTF("\r\nWrite successfully.");
+        PRINTF("Sync Write velocity base target position successfully.\r\n");
     }
     else
     {
         PRINTF("\r\nFailed to send data.");
     }
-    Sleep(1);
+    Sleep(1000);
 
-    if (serialPort.Read(pack,&bytesRead))
-    {
-        ret = servo_set_velocity_base_target_dec_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to read data.");
-    }
-    Sleep(20);
+    //在控速模式下，让ID1以360°/s的控速目标速度运动到300°位置，让ID2以720°/s的控速目标速度运动到150°位置
 
-    //设置舵机的控速目标位置
-    servo_set_velocity_base_target_position(1, 0, order_buffer,&order_len);
-    if (serialPort.Write(order_buffer, order_len, &bytesWritten)) {
-        PRINTF("\r\nWrite successfully.");
+    //id为1，2的舵机速度分别设置为3600，7200，位置分别设置为3000，1500
+    servo.velocity[0] = 3600;
+    servo.velocity[1] = 7200;
+    servo.position[0] = 3000;
+    servo.position[1] = 1500;
+
+    servo_sync_write_velocity_base_target_position_and_velocity(servo, order_buffer, &order_len);
+    if (serialPort.Write(order_buffer, order_len, &bytesWritten))
+    {
+        PRINTF("Sync Write velocity base target position and velocity successfully.\r\n");
     }
     else
     {
         PRINTF("\r\nFailed to send data.");
     }
-    Sleep(1);
+    Sleep(1000);
 
-    if (serialPort.Read(pack,&bytesRead))
+    //在控速模式下，让ID1舵机以720°/s的控速目标速度、500°/s²的控速目标加速度、50°/s²的控速目标减速度运动到0°位置，让ID2舵机以360°/s的控速目标速度、50°/s²的控速目标加速度、500°/s²的控速目标减速度运动到300°位置
+
+    //id为1，2的舵机速度分别设置为7200，3600，位置分别设置为0，3000,加速度分别设置为10，1，减速度分别设置为1，10
+    servo.velocity[0] = 7200;
+    servo.velocity[1] = 3600;
+    servo.position[0] = 0;
+    servo.position[1] = 3000;
+    servo.acc_velocity[0] = 10;
+    servo.acc_velocity[1] = 1;
+    servo.dec_velocity[0] = 1;
+    servo.dec_velocity[1] = 10;
+
+    servo_sync_write_velocity_base_target_acc_dec_velocity_and_position(servo, order_buffer, &order_len);
+    if (serialPort.Write(order_buffer, order_len, &bytesWritten))
     {
-        ret = servo_set_velocity_base_target_position_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
+        PRINTF("Sync Write velocity base target acc,dec,velocity and position successfully.\r\n");
     }
     else
     {
-        PRINTF("\r\nFailed to read data.");
+        PRINTF("\r\nFailed to send data.");
     }
     Sleep(1000);
 
