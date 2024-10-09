54e2e8a843c137086e507d7d420b89250cdea8d6
王文龙 <2641090294@qq.com>
2024-10-09 11:48:58 +0800

    10.9 修改C++ Demo

--- C++/Windows/PrimaryMemoryTable/Demo/CurrentBase/main.cpp
+++ C++/Windows/PrimaryMemoryTable/Demo/CurrentBase/main.cpp
@@ -5,119 +5,94 @@
 int main()
 {
     uint8_t ret;                        //Status Flag
-    uint8_t order_buffer[20] = {0};     //Store Generated Instructions
+    uint8_t order_buffer[20] = { 0 };     //Store Generated Instructions
     uint8_t order_len = 0;              //Instruction Length
-    uint8_t pack[20] = {0};             //Store the received status packet
+    uint8_t pack[20] = { 0 };             //Store the received status packet
 
     CSerialPort serialPort;             //Create a serial port class.
 
     DWORD bytesRead;                    //The actual number of bytes read from the serial port.
     DWORD bytesWritten;                 //The actual number of bytes written to the serial port.
 
-    if (serialPort.Open(14, 1000000))
+    if (serialPort.Open(18, 1000000))
     {
-        PRINTF("\r\nOpen Serial successfully.");
+        PRINTF("Open Serial successfully.\r\n");
     }
     else
     {
-        PRINTF("\r\nFailed to open serial port.");
+        PRINTF("Failed to open serial port.\r\n");
         return -1;
     }
 
     //Change the torque switch of servo ID1 to OFF.
-    servo_set_torque_switch(1, 0, order_buffer,&order_len);
+    primary_servo_set_torque_switch(1, 0, order_buffer, &order_len);
 
-    if (serialPort.Write(order_buffer, order_len, &bytesWritten)) {
-        PRINTF("\r\nWrite successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to send data.");
-    }
+    serialPort.Write(order_buffer, order_len, &bytesWritten);
     Sleep(1);
 
-    if (serialPort.Read(pack,&bytesRead))
+    if (serialPort.Read(pack, &bytesRead))
     {
-        ret = servo_set_torque_switch_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
+        ret = primary_servo_set_torque_switch_analysis(pack);
+        if (ret == PRIMARY_SUCCESS)
+            PRINTF("set torque switch successful.\r\n");
     }
     else
     {
-        PRINTF("\r\nFailed to read data.");
+        PRINTF("Failed to read data.\r\n");
     }
     Sleep(20);
 
     //Change the control mode of servo ID1 to the current control mode.
-    servo_set_control_mode(1, 2, order_buffer,&order_len);
+    primary_servo_set_control_mode(1, 2, order_buffer, &order_len);
 
-    if (serialPort.Write(order_buffer, order_len, &bytesWritten)) {
-        PRINTF("\r\nWrite successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to send data.");
-    }
+    serialPort.Write(order_buffer, order_len, &bytesWritten);
     Sleep(1);
 
-    if (serialPort.Read(pack,&bytesRead))
+    if (serialPort.Read(pack, &bytesRead))
     {
-        ret = servo_set_control_mode_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
+        ret = primary_servo_set_control_mode_analysis(pack);
+        if (ret == PRIMARY_SUCCESS)
+            PRINTF("set control mode successful.\r\n");
     }
     else
     {
-        PRINTF("\r\nFailed to read data.");
+        PRINTF("Failed to read data.\r\n");
     }
     Sleep(20);
 
     //Change the torque switch of servo ID1 to ON.
-    servo_set_torque_switch(1, 1, order_buffer,&order_len);
+    primary_servo_set_torque_switch(1, 1, order_buffer, &order_len);
 
-    if (serialPort.Write(order_buffer, order_len, &bytesWritten)) {
-        PRINTF("\r\nWrite successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to send data.");
-    }
+    serialPort.Write(order_buffer, order_len, &bytesWritten);
     Sleep(1);
 
-    if (serialPort.Read(pack,&bytesRead))
+    if (serialPort.Read(pack, &bytesRead))
     {
-        ret = servo_set_torque_switch_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
+        ret = primary_servo_set_torque_switch_analysis(pack);
+        if (ret == PRIMARY_SUCCESS)
+            PRINTF("set torque switch successful.\r\n");
     }
     else
     {
-        PRINTF("\r\nFailed to read data.");
+        PRINTF("Failed to read data.\r\n");
     }
     Sleep(20);
 
     //Change the target PWM of servo ID1 to 100mA.
-    servo_set_target_current(1, 100, order_buffer,&order_len);
+    primary_servo_set_target_current(1, 100, order_buffer, &order_len);
 
-    if (serialPort.Write(order_buffer, order_len, &bytesWritten))
-    {
-        PRINTF("\r\nWrite successfully.");
-    }
-    else
-    {
-        PRINTF("\r\nFailed to send data.");
-    }
+    serialPort.Write(order_buffer, order_len, &bytesWritten);
     Sleep(1);
 
-    if (serialPort.Read(pack,&bytesRead))
+    if (serialPort.Read(pack, &bytesRead))
     {
-        ret = servo_set_target_current_analysis(pack);
-        if(ret == SUCCESS)
-            PRINTF("\r\nSet successfully.");
+        ret = primary_servo_set_target_current_analysis(pack);
+        if (ret == PRIMARY_SUCCESS)
+            PRINTF("set target current successful.\r\n");
     }
     else
     {
-        PRINTF("\r\nFailed to read data.");
+        PRINTF("Failed to read data.\r\n");
     }
     Sleep(3000);
 
